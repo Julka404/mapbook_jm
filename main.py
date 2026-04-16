22ffe9d1728eccf5879cbdf114265d7d4d611951
Julka404 <julia.mastalerz@student.wat.edu.pl>
2026-04-16 14:00:28 +0200

    wip: mvc teamplate applied

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-users: list = [
-    {'username': 'oliwia', 'location': 'łódź', 'posts': 1,
-     'usermessage': ['życzenia1', 'kocham legie', 'sprzedam opla', 'kiwi']},
-    {'username': 'paweł', 'location': 'ostróda', 'posts': 2,
-     'usermessage': ['życzenia2', 'kocham legie', 'sprzedam opla']},
-    {'username': 'eliza', 'location': 'radom', 'posts': 3, 'usermessage': ['życzenia3', 'kocham legie']},
-    {'username': 'filip', 'location': 'dęblin', 'posts': 4,
-     'usermessage': ['życzenia4', 'kocham legie', 'sprzedam opla', 'kiwi3']}
-]
-
-
-def read_data(users_data: list) -> None:
-    for user in users_data:
-        print(
-            f'twój znajowmy {user['username']} z miejscowości {user["location"]} opublikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermessage'][-1]}')
-
+from mapbook_lib.model import users
+from mapbook_lib.controller import read_data
 
 
 read_data(users[1:])
